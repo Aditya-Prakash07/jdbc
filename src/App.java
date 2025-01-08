56a4de78a3e846d5eddfa7b06ff8d43f8cb9227f
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-08 12:52:31 +0530

    jdbc

--- src/App.java
+++ src/App.java
@@ -1,3 +1,15 @@
+
+// Java database connectivity 
+// 1. import --> java.sql
+// 2. load and register the driver --> com.mysql.jdbc.driver
+// 3. create connection
+// 4. create a statement 
+// 5. execute the query
+// 6. process the result
+// 7. close
+
+
+import java.sql.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
