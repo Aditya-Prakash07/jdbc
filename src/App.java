e5c65d6eacbcb39c8006db387e64f7b1dd0466a0
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-08 14:08:24 +0530

    modified txt file

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 
 // Java database connectivity 
 // 1. import --> java.sql
-// 2. load and register the driver --> com.mysql.jdbc.driver
+// 2. load and register the driver --> com.mysql.cj.jdbc.Driver
 // 3. create connection
 // 4. create a statement 
 // 5. execute the query
@@ -10,8 +10,24 @@
 
 
 import java.sql.*;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String url = "jdbc:mysql://127.0.0.1:3306/aditya";
+        String username = "root";
+        String pass = "admin123";
+        String query = "SELECT * FROM aditya.student WHERE rollno = 1";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, pass);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        rs.next();
+        String name = rs.getString("name");
+        System.out.println(name);
+        sc.close();
+        st.close();
+        con.close();
+        
     }
 }
