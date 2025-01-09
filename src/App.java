1d1839abec953355a3de0001387e32faa30c4bf0
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-09 23:52:25 +0530

    Intro to DAO-----> Data Access Object

--- src/App.java
+++ src/App.java
@@ -166,38 +166,90 @@
 
 
 
+// //Class.forName()...method--understanding
+
+
+// import java.sql.DriverManager;
+
+// class Pqr{
+//     //Static Block
+//     static{
+//         System.out.println("Inside Static Block");
+//     }
+//     // Instance Block
+//     {
+//         System.out.println("Inside Instance Block");
+//     }
+// }
+
+// public class App {
+
+//     public static void main(String[] args) throws Exception{
+//        // Pqr pqr = new Pqr(); // whenever we create an object so before creating the object it will first load the class (static method will be called), while we are creating the object it will call the instance block .
+//         //what if we don't want to create the object but still i want to call the static block in this scenerio we can just load the class using the Class.forName(class_name);
+//         //Class.forName("Pqr");// class loading
+//         //Class.forName("Pqr").getDeclaredConstructor().newInstance(); // class loading + instance creation
+
+//         //tow ways to load the driver 
+//         //1.
+//         Class.forName("com.mysql.cj.jdbc.Driver"); // Preffered
+//         //2.
+//         DriverManager.registerDriver("com.mysql.cj.jdbc.Driver");
+//         // both 1 & 2 are same thats because whenever you go to the driver class of mysql connecter in this you have a static block and this static block does the same thing that we are trying to do in 2nd type. so whenever we call the Class.forName() it loads the class and whenever it loads it will execute the static block . and whenever it executes the static block it will call the method register driver , That means we do not need to write the statement in 2nd type. Because we are writing Class.forName()..
+
+//         // So that means whenever we try to register the driver the Class.forName() is not the method which register the driver. Its actually the static block of driver class which register the driver.
+
+//     }
+// }
+
+
+
+
+
+
+
 //DAO(data access object ) it is a designed layer it is always prefferd to write your data base related stuffs inside the dao layer only
 
-import java.sql.DriverManager;
 
-class Pqr{
-    //Static Block
-    static{
-        System.out.println("Inside Static Block");
-    }
-    // Instance Block
-    {
-        System.out.println("Inside Instance Block");
+import java.sql.*;
+
+class Student{
+    int rollno;
+    String name;
+    
+}
+class StudentDao{
+    public Student getStudent(int rollno){
+        String url = "jdbc:mysql://127.0.0.1:3306/aditya";
+        String username = "root";
+        String pass = "admin123";
+        try{
+            String query = "SELECT name FROM student WHERE rollno = "+rollno;
+            Student s = new Student();
+            s.rollno = rollno;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, username, pass);
+           
+            Statement st = con.createStatement();
+            
+            ResultSet rs = st.executeQuery(query);
+            rs.next();
+            String name = rs.getString(1);
+            s.name = name;
+            return s;
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+        return null;
     }
 }
-
 public class App {
 
     public static void main(String[] args) throws Exception{
-       // Pqr pqr = new Pqr(); // whenever we create an object so before creating the object it will first load the class (static method will be called), while we are creating the object it will call the instance block .
-        //what if we don't want to create the object but still i want to call the static block in this scenerio we can just load the class using the Class.forName(class_name);
-        //Class.forName("Pqr");// class loading
-        //Class.forName("Pqr").getDeclaredConstructor().newInstance(); // class loading + instance creation
-
-        //tow ways to load the driver 
-        //1.
-        Class.forName("com.mysql.cj.jdbc.Driver"); // Preffered
-        //2.
-        DriverManager.registerDriver("com.mysql.cj.jdbc.Driver");
-        // both 1 & 2 are same thats because whenever you go to the driver class of mysql connecter in this you have a static block and this static block does the same thing that we are trying to do in 2nd type. so whenever we call the Class.forName() it loads the class and whenever it loads it will execute the static block . and whenever it executes the static block it will call the method register driver , That means we do not need to write the statement in 2nd type. Because we are writing Class.forName()..
-        
-        // So that means whenever we try to register the driver the Class.forName() is not the method which register the driver. Its actually the static block of driver class which register the driver.
-
+       StudentDao dao = new StudentDao();
+       Student s1 = dao.getStudent(12);
+      // StudentDao.addStudent("Nanhak"); // Implement this by your self
+       System.out.println(s1.name);
     }
 }
-
